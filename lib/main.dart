50b6578e2a98eef08a285a9bab7e4ab63824e78d
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-06-08 17:08:32 +0900

    feat : riverpod state provider example

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_challenge_flutter/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
+    const ProviderScope(
+      // 상태관리가 필요한 위젯을 ProviderScope로 감싸야 사용가능
+
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
     ),
   );
 }
--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -1,17 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_challenge_flutter/layout/default_layout.dart';
+import 'package:riverpod_challenge_flutter/riverpod/state_provider_screen.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
+  // stateWidger 은 ConsumerWidget 으로 바꿔 riverpod 사용
   const StateProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    //WidgetRef ref 파라미터 필요 (값 접근할때 사용)
+
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
       title: "StateProviderScreen",
-      body: Column(
-        children: const [],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(numberProvider.notifier)
+                    .update((state) => state + 1); //update 하는 방법
+              },
+              child: const Text(
+                'UP',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const _NextScreen(),
+                  ),
+                );
+              },
+              child: const Text(
+                'Next Screen',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: "StateProviderScreen",
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: const Text(
+                'UP',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
