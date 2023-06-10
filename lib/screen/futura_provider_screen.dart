0e53ee0ab74c4b8865ea5f99b66aecbdf9376f99
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-06-10 14:34:19 +0900

    future provider

---
+++ lib/screen/futura_provider_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_challenge_flutter/layout/default_layout.dart';
+import 'package:riverpod_challenge_flutter/riverpod/future_provider.dart';
+
+class FutureProviderScreen extends ConsumerWidget {
+  const FutureProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //AsyncValue
+    final state = ref.watch(multiplesFutureProvider);
+
+    return DefaultLayout(
+      title: "FutureProviderScreen",
+      body: Column(
+        children: [
+          state.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+              );
+            },
+            error: (err, stack) {
+              return Text(
+                err.toString(),
+              );
+            },
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
